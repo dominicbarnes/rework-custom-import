b440be9e27d912904e1d20d5fba303ce51b28015
Dominic Barnes <dominic@dbarnes.info>
2016-01-01 00:34:08 -0800

    adding debug

--- index.js
+++ index.js
@@ -3,16 +3,18 @@
 
 let assert = require('assert');
 let css = require('css');
+let debug = require('debug')('rework-custom-import');
 let parse = require('parse-import');
 
 module.exports = function (mapping) {
+  debug('initialize');
   assert(mapping, 'you must supply a precomputed mapping');
-  assert(typeof mapping === 'object', 'you must supply a precomputed mapping');
 
   return function run(style) {
     let source = getSource(style);
     let meta = mapping[source];
     let rules = [];
+    debug('running on %s', source);
 
     style.rules.forEach(function (rule) {
       if (rule.type !== 'import') {
@@ -20,12 +22,14 @@ module.exports = function (mapping) {
         return;
       }
 
+      debug('importing %s', rule.import);
       let data = parse(`@import ${rule.import};`).shift();
       let dep = mapping[meta.deps[data.path]];
       let content = css.parse(dep.source, { source: dep.id }).stylesheet;
       run(content);
 
       if (data.condition && data.condition.length) {
+        debug('import condition: %j', data.condition);
         rules.push({
           media: data.condition,
           rules: content.rules,
